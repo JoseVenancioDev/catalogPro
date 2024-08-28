2b6f2d809c606c1e6ad8be351d14d1a20c3ddf3d
JoseVenancioDev <josevenanciodev@gmail.com>
2024-08-28 08:48:36 -0300

    Esboçando projeto

--- catalogPro/src/pages/CadProdutos.jsx
+++ catalogPro/src/pages/CadProdutos.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
 export function CadProdutos() {
+    function formatarValor() {
+        const valorInput = document.getElementById('valor').value;
+        const valor = parseFloat(valorInput);
+        if (!isNaN(valor)) {
+            const formatador = new Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+            });
+            document.getElementById('resultado').textContent = formatador.format(valor);
+        } else {
+            document.getElementById('resultado').textContent = 'Valor inválido';
+        }
+    }
     return (
-        <div>
-            <h1>Cadastro de produtos</h1>
+        <div className="conteudo">
+            <form action="" method="post">
+                <input type="file" name="fotoProduto" id="fotoProduto" />
+                <br />
+                <input type="text" name="produto" placeholder="Nome do produto"/>
+            </form>
         </div>
     );
 }
