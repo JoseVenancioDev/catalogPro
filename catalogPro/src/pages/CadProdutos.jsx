13f0cc09355cfbbe41666255af88ea0fd28d360b
JoseVenancioDev <josevenanciodev@gmail.com>
2024-08-29 10:31:52 -0300

    Resolvendo conflitos

--- catalogPro/src/pages/CadProdutos.jsx
+++ catalogPro/src/pages/CadProdutos.jsx
@@ -1,26 +1,9 @@
 import React from 'react';
 
 export function CadProdutos() {
-    function formatarValor() {
-        const valorInput = document.getElementById('valor').value;
-        const valor = parseFloat(valorInput);
-        if (!isNaN(valor)) {
-            const formatador = new Intl.NumberFormat('pt-BR', {
-                style: 'currency',
-                currency: 'BRL'
-            });
-            document.getElementById('resultado').textContent = formatador.format(valor);
-        } else {
-            document.getElementById('resultado').textContent = 'Valor inválido';
-        }
-    }
     return (
-        <div className="conteudo">
-            <form action="" method="post">
-                <input type="file" name="fotoProduto" id="fotoProduto" />
-                <br />
-                <input type="text" name="produto" placeholder="Nome do produto"/>
-            </form>
+        <div>
+            <h1>Cadastro de produtos</h1>
         </div>
     );
 }
