f58e74cea251c6f15335ce92ab01f2c6f5161d06
JoseVenancioDev <josevenanciodev@gmail.com>
2024-09-04 08:47:12 -0300

    Limitando o recentes a 5

--- catalogPro/src/components/VerProduto/index.jsx
+++ catalogPro/src/components/VerProduto/index.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './VerProduto.css';
 
 export const VerProduto = ({ produtos = [] }) => {
+    const [produtosExibidos, setProdutosExibidos] = useState([]);
+
+    useEffect(() => {
+        if (produtos.length > 0) {
+            setProdutosExibidos(prevProdutos => {
+                // Se a lista já tem 5 produtos, remove o primeiro
+                const produtosAtualizados = prevProdutos.length === 5
+                    ? [...prevProdutos.slice(1), produtos[produtos.length - 1]]
+                    : [...prevProdutos, produtos[produtos.length - 1]];
+
+                return produtosAtualizados;
+            });
+        }
+    }, [produtos]);
+
     return (
         <div className='produtos-recentes'>
             <h3>Produtos Recentes</h3>
             <div className="lista-recentes">
-                {produtos.length === 0 ? (
+                {produtosExibidos.length === 0 ? (
                     <p>Não há produtos!</p>
                 ) : (
                     <ul>
-                        {produtos.map((produto, index) => (
+                        {produtosExibidos.map((produto, index) => (
                             <li key={index}>
                                 <h4>{produto.nome}</h4>
                                 {produto.foto && (
