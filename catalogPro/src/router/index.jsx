e3ea77d635ce6a29f2652d51df064d8195aeac31
alice-silva <4lice.m4rtins@gmail.com>
2024-09-04 08:44:41 -0300

    Commit Completo

--- catalogPro/src/router/index.jsx
+++ catalogPro/src/router/index.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { PagInicial } from '../pages/PagInicial';
 import { Login } from '../pages/Login';
 import { CadProdutos } from '../pages/CadProdutos';
+import { Cadastro } from '../pages/Cadastro';
 
 export const RouteAsRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<PagInicial />} />
-                <Route path="/bemvindo" element={<Login />} />
+                <Route path="/bemvindo" element={<Cadastro />} />
+                <Route path="/login" element={<Login />} />
                 <Route path="/produtos" element={<CadProdutos />} />
             </Routes>
         </BrowserRouter>
