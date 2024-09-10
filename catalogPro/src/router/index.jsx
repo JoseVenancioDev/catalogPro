7102bd0a9360e5499e5b57a68b93e96aa02aa970
alice-silva <4lice.m4rtins@gmail.com>
2024-09-10 07:49:07 -0300

    aaa

--- catalogPro/src/router/index.jsx
+++ catalogPro/src/router/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { PagInicial } from '../pages/PagInicial';
-import { Login } from '../pages/Login';
+import { Login } from '../components/Login/Login';
 import { CadProdutos } from '../pages/CadProdutos';
+import { Cadastro } from '../components/Cadastro/Cadastro';
 
 export const RouteAsRouter = () => {
     return (
