e7738f8c75fb89613e3d309103282f9571b7b1ba
José Venâncio <josevenanciodev@gmail.com>
2024-09-21 20:54:07 -0300

    Ajustando a folha de estilo

--- catalogPro/src/pages/TelaProdutos.jsx
+++ catalogPro/src/pages/TelaProdutos.jsx
@@ -6,15 +6,23 @@ import { Acessos } from '../components/Acessos';
 
 export const TelaProdutos = () => {
     const [produtos, setProdutos] = useState([]);
+    const [isCollapsed, setIsCollapsed] = useState(false);
 
     const adicionarProduto = (produto) => {
         setProdutos([...produtos, produto]);
     };
 
+    const toggleSidebar = () => {
+        setIsCollapsed(!isCollapsed);
+    };
+
     return (
-        <div className="main-container">
+        <div className={`main-container ${isCollapsed ? 'collapsed' : ''}`}>
             <Acessos />
-            <div className="content">
+            <button className="sidebar-toggle-btn" onClick={toggleSidebar}>
+                {isCollapsed ? '>' : '<'}
+            </button>
+            <div className={`content ${isCollapsed ? 'collapsed' : ''}`}>
                 <FormProduto adicionarProduto={adicionarProduto} />
                 <VerProduto produtos={produtos} />
             </div>
